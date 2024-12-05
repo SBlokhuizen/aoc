ee820534ade6cd4f5bfca11420ff8e968d9fa3bd
Sebbe <sblokhuizen@ignitioncomputing.com>
2024-12-05 09:56:26 +0100

    day5 complete

--- 2024/day5/src/main.rs
+++ 2024/day5/src/main.rs
@@ -9,11 +9,34 @@ fn fill_hashmap(rules: String) -> HashMap<i32, (Vec<i32>, Vec<i32>)> {
     }
     lookup_table
 }
-fn part1(rules: String, updates: String) -> i32 {
+
+fn sort_invalid(
+    numbers: &mut Vec<i32>,
+    lookup_table: &HashMap<i32, (Vec<i32>, Vec<i32>)>,
+) -> Vec<i32> {
+    for i in 0..numbers.len() {
+        for j in i + 1..numbers.len() {
+            if let Some((before, _)) = lookup_table.get(&numbers[i]) {
+                if before.contains(&numbers[j]) {
+                    numbers.swap(i, j)
+                }
+            }
+            if let Some((_, after)) = lookup_table.get(&numbers[j]) {
+                if after.contains(&numbers[i]) {
+                    numbers.swap(i, j)
+                }
+            }
+        }
+    }
+    numbers.to_vec()
+}
+
+fn part1(rules: String, updates: String) -> (i32, i32) {
     let lookup_table = fill_hashmap(rules);
-    let mut total = 0;
+    let mut total1 = 0;
+    let mut total2 = 0;
     for line in updates.lines() {
-        let numbers: Vec<i32> = line.split(',').map(|s| s.parse().unwrap()).collect();
+        let mut numbers: Vec<i32> = line.split(',').map(|s| s.parse().unwrap()).collect();
         let mut is_valid = true;
         for i in 0..numbers.len() {
             for j in i + 1..numbers.len() {
@@ -30,10 +53,13 @@ fn part1(rules: String, updates: String) -> i32 {
             }
         }
         if is_valid {
-            total += numbers[numbers.len() / 2];
+            total1 += numbers[numbers.len() / 2];
+        } else {
+            let numbers_sorted = sort_invalid(&mut numbers, &lookup_table);
+            total2 += numbers_sorted[numbers_sorted.len() / 2];
         }
     }
-    total
+    (total1, total2)
 }
 
 fn add_relationship(
@@ -48,12 +74,10 @@ fn add_relationship(
 fn main() {
     let file_name = "../input/day5.txt";
     let data = fs::read_to_string(file_name).expect("Unable to read file");
-    // Split into two parts by the first empty line
     let mut parts = data.splitn(2, "\n\n");
-
-    // Assign the two parts to separate variables
     let rules = parts.next().unwrap_or("").trim();
     let updates = parts.next().unwrap_or("").trim();
-    let total1 = part1(rules.to_string(), updates.to_string());
-    println!("{total1}");
+
+    let (total1, total2) = part1(rules.to_string(), updates.to_string());
+    println!("{total1}\n{total2}");
 }
