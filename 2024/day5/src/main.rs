84650c087cf5aab9d8c36f61206216ab1e843f21
Sebbe <sblokhuizen@ignitioncomputing.com>
2024-12-05 09:39:35 +0100

    part1

---
+++ 2024/day5/src/main.rs
@@ -0,0 +1,59 @@
+use std::collections::HashMap;
+use std::fs;
+
+fn fill_hashmap(rules: String) -> HashMap<i32, (Vec<i32>, Vec<i32>)> {
+    let mut lookup_table: HashMap<i32, (Vec<i32>, Vec<i32>)> = HashMap::new();
+    for line in rules.lines() {
+        let parts: Vec<i32> = line.split('|').map(|s| s.parse().unwrap()).collect();
+        add_relationship(&mut lookup_table, parts[0], parts[1]);
+    }
+    lookup_table
+}
+fn part1(rules: String, updates: String) -> i32 {
+    let lookup_table = fill_hashmap(rules);
+    let mut total = 0;
+    for line in updates.lines() {
+        let numbers: Vec<i32> = line.split(',').map(|s| s.parse().unwrap()).collect();
+        let mut is_valid = true;
+        for i in 0..numbers.len() {
+            for j in i + 1..numbers.len() {
+                if let Some((before, _)) = lookup_table.get(&numbers[i]) {
+                    if before.contains(&numbers[j]) {
+                        is_valid = false;
+                    }
+                }
+                if let Some((_, after)) = lookup_table.get(&numbers[j]) {
+                    if after.contains(&numbers[i]) {
+                        is_valid = false;
+                    }
+                }
+            }
+        }
+        if is_valid {
+            total += numbers[numbers.len() / 2];
+        }
+    }
+    total
+}
+
+fn add_relationship(
+    lookup_table: &mut HashMap<i32, (Vec<i32>, Vec<i32>)>,
+    before: i32,
+    after: i32,
+) {
+    lookup_table.entry(before).or_default().1.push(after);
+    lookup_table.entry(after).or_default().0.push(before);
+}
+
+fn main() {
+    let file_name = "../input/day5.txt";
+    let data = fs::read_to_string(file_name).expect("Unable to read file");
+    // Split into two parts by the first empty line
+    let mut parts = data.splitn(2, "\n\n");
+
+    // Assign the two parts to separate variables
+    let rules = parts.next().unwrap_or("").trim();
+    let updates = parts.next().unwrap_or("").trim();
+    let total1 = part1(rules.to_string(), updates.to_string());
+    println!("{total1}");
+}
