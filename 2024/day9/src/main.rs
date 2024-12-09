f6e2236a08e24379564c9183f723ecc32d6b7fd6
Sebbe <sblokhuizen@ignitioncomputing.com>
2024-12-09 16:20:37 +0100

    day9: finish part1

--- 2024/day9/src/main.rs
+++ 2024/day9/src/main.rs
@@ -1,7 +1,7 @@
 use std::fs;
 
-fn expand(data: &String) -> Vec<char> {
-    let mut expansion: Vec<char> = Default::default();
+fn expand(data: &String) -> Vec<String> {
+    let mut expansion: Vec<String> = Default::default();
     let mut is_block = true;
     let mut num: u64;
     let mut id = 0;
@@ -10,15 +10,13 @@ fn expand(data: &String) -> Vec<char> {
         if is_block {
             for _ in 0..num {
                 let id_str = id.to_string();
-                for ch in id_str.chars() {
-                    expansion.push(ch);
-                }
+                expansion.push(id_str);
             }
             is_block = false;
             id += 1;
         } else {
             for _ in 0..num {
-                expansion.push('.');
+                expansion.push(".".to_string());
             }
             is_block = true;
         }
@@ -26,41 +24,42 @@ fn expand(data: &String) -> Vec<char> {
     expansion
 }
 
-fn compress(expansion: &mut Vec<char>) {
+fn compress(expansion: &mut Vec<String>) {
     let mut front_ptr: usize = 0;
     let mut back_ptr: usize = expansion.len() - 1;
     while front_ptr < back_ptr {
-        while expansion[front_ptr] != '.' {
+        while expansion[front_ptr] != "." {
             front_ptr += 1;
         }
-        while expansion[back_ptr] == '.' {
+        while expansion[back_ptr] == "." {
             back_ptr -= 1;
         }
         expansion.swap(front_ptr, back_ptr);
     }
     expansion.swap(front_ptr, back_ptr);
-    //println!("{:?}", expansion);
-    //println!("{} {}", expansion[front_ptr], expansion[back_ptr]);
+}
+
+fn calc_checksum(expansion: &mut Vec<String>) -> u64 {
+    let mut id = 0;
+    let mut num: u64;
+    let mut checksum = 0;
+    for string in expansion.iter() {
+        if string != "." {
+            num = string
+                .parse::<u64>()
+                .expect("Failed to parse string as u64");
+            checksum += num * id;
+            id += 1;
+        }
+    }
+    checksum
 }
 
 fn part1(data: &String) {
     let mut expansion = expand(data);
-    println!("{:?}", expansion);
     compress(&mut expansion);
-    println!("{:?}", expansion);
-    let mut id = 0;
-    let mut num: u64;
-    let mut total = 0;
-    for &ch in expansion.iter() {
-        if ch != '.' {
-            num = (ch as u64) - ('0' as u64);
-            //println!("{} {}", num, id);
-            total += num * id;
-            id += 1;
-        }
-    }
-
-    println!("{}", total);
+    let checksum = calc_checksum(&mut expansion);
+    println!("{}", checksum);
 }
 
 fn main() {
