f31df5eb59c25bb4b5391a783cc634e662520fa3
Sebbe <sblokhuizen@ignitioncomputing.com>
2024-12-11 19:38:40 +0100

    day 11 part 2

--- 2024/day11/src/main.rs
+++ 2024/day11/src/main.rs
@@ -1,47 +1,64 @@
+use std::collections::HashMap;
 use std::fs;
-fn blink(stones: &mut Vec<String>) {
-    let mut new_elements = Vec::new();
 
-    for (i, stone) in stones.iter_mut().enumerate() {
-        if *stone == "0" {
-            *stone = "1".to_string();
-        } else if stone.len() % 2 == 0 {
-            let stone_size = stone.len() / 2;
-            let first_stone = stone[..stone_size]
+fn blink(stones: &mut HashMap<String, u64>) {
+    let mut new_stones: HashMap<String, u64> = HashMap::new();
+    for (key, value) in stones.iter() {
+        if key == "0" {
+            let current_new_value = new_stones.get("1").unwrap_or(&0);
+
+            new_stones.insert("1".to_string(), current_new_value + value);
+        } else if key.len() % 2 == 0 {
+            let stone_size = key.len() / 2;
+            let first_stone = key[..stone_size]
                 .parse::<u64>()
                 .map(|n| n.to_string())
                 .unwrap_or_default();
-            let second_stone = stone[stone_size..]
+            let second_stone = key[stone_size..]
                 .parse::<u64>()
                 .map(|n| n.to_string())
                 .unwrap_or_default();
 
-            *stone = first_stone;
-            new_elements.push((i + 1, second_stone));
-        } else {
-            if let Ok(parsed_number) = stone.parse::<u64>() {
-                let result = parsed_number * 2024;
-                *stone = result.to_string();
+            if let Some(curr_first_stone) = new_stones.get(&first_stone) {
+                new_stones.insert(first_stone.to_string(), *curr_first_stone + value);
+            } else {
+                new_stones.insert(first_stone.to_string(), *value);
             }
+
+            if let Some(curr_second_stone) = new_stones.get(&second_stone) {
+                new_stones.insert(second_stone.to_string(), *curr_second_stone + value);
+            } else {
+                new_stones.insert(second_stone.to_string(), *value);
+            }
+        } else {
+            let num: u64 = key.parse().expect("Failed to parse string to u64");
+            let new_key = (num * 2024).to_string();
+            let current_new_value = new_stones.get(&new_key).unwrap_or(&0);
+            new_stones.insert(new_key, current_new_value + value);
         }
     }
-
-    for (index, element) in new_elements {
-        stones.insert(index, element);
-    }
-    //println!("{:?}", stones);
+    *stones = new_stones;
 }
-fn part1(data: &String) {
-    let mut stones: Vec<String> = data.split_whitespace().map(|s| s.to_string()).collect();
-    //println!("{:?}", stones);
-    for i in 0..25 {
-        println!("{i}");
+
+fn solve(data: &String) {
+    let mut stones: HashMap<String, u64> = HashMap::new();
+    let init_stones: Vec<String> = data.split_whitespace().map(String::from).collect();
+
+    for stone in &init_stones {
+        stones.insert(stone.to_string(), 1);
+    }
+
+    for _ in 0..25 {
         blink(&mut stones);
     }
-    println!("{}", stones.len());
+    println!("{}", stones.values().sum::<u64>());
+    for _ in 25..75 {
+        blink(&mut stones);
+    }
+    println!("{}", stones.values().sum::<u64>());
 }
 fn main() {
     let file_name = "../input/day11.txt";
     let data = fs::read_to_string(file_name).expect("Unable to read file");
-    part1(&data);
+    solve(&data);
 }
